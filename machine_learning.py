21d07fcbf81913575f94de26ba22d8cfab09ae38
Konstantin Kobs <mail@konstantinkobs.de>
2020-11-03 21:14:25 +0100

    SentenceBERT for Sentence Embeddings and T-SNE for Dimensionality Reduction

--- machine_learning.py
+++ machine_learning.py
@@ -1,46 +1,47 @@
 from bs4 import BeautifulSoup
 import json
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-# from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.decomposition import TruncatedSVD
-# from sklearn.manifold import TSNE
+from sklearn.manifold import TSNE
 import plotly.graph_objects as go
-# import spacy
-# import numpy as np
-import umap
+from flair.data import Sentence
+from flair.embeddings import SentenceTransformerDocumentEmbeddings
+from sentence_transformers import util
+from tqdm import tqdm
 
-file_path = "rss/2020/10/29/rss_de.json"
+embedder = SentenceTransformerDocumentEmbeddings(
+    'distiluse-base-multilingual-cased-v2')
+
+file_path = "rss/2020/11/1/rss_de.json"
 
 with open(file_path, 'r') as f:
     entries = json.load(f)
 
 titles = [e['title'] for e in entries]
-# summaries = [e['summary'] for e in entries]
-tags = [[t["term"] for t in e['tags']] if "tags" in e else [] for e in entries]
+summaries = [e['summary'] for e in entries]
+# tags = [[t["term"] for t in e['tags']] if "tags" in e else [] for e in entries]
 
-entries = [f"{t} {' '.join(tags)}" for t, tags in zip(titles, tags)]
-# entries = [f"{t} {s} {' '.join(tags)}" for t, s, tags in zip(titles, summaries, tags)]
-entries = [BeautifulSoup(e, "html.parser").get_text() for e in entries]
+data = [f"{t}. {s}" for t, s in zip(titles, summaries)]
+# Clean data by filtering HTML tags
+data = [BeautifulSoup(d, features="lxml").get_text() for d in data]
 
+entries = [Sentence(d, use_tokenizer=True) for d in tqdm(data)]
 
-# nlp = spacy.load("de_core_news_sm")
-# X = np.array([np.mean([t.vector for t in nlp(e)], axis=0) for e in entries])
+# calculate embeddings
+for s in tqdm(entries):
+    embedder.embed(s)
 
-# vectorizer = CountVectorizer(binary=True)
-vectorizer = TfidfVectorizer()
-X = vectorizer.fit_transform(entries)
-# print(vectorizer.vocabulary_)
+embeddings = [s.embedding.detach().numpy() for s in entries]
 
-# dim_red = TruncatedSVD(n_components=2)
-dim_red = umap.UMAP()
+reducer = TSNE(n_components=2)
 
-X_trans = dim_red.fit_transform(X)
+X_trans = reducer.fit_transform(embeddings)
+
 
 def color(title):
     if "corona" in title.lower():
         return "#ff0000"
     else:
         return "#000000"
+
 
 fig = go.Figure(data=go.Scatter(x=X_trans[:, 0],
                                 y=X_trans[:, 1],
