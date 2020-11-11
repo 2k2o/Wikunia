50c059511bbb81e2fdc36b185974e486f809a6dd
Konstantin Kobs <mail@konstantinkobs.de>
2020-11-11 08:59:16 +0100

    show headlines and summaries as a grid

--- js/app.js
+++ js/app.js
@@ -1,21 +1,6 @@
-const svg = d3.select("#scatter");
-
-const dimensions = svg.node().getBoundingClientRect();
-const width = dimensions.width;
-const height = dimensions.height;
-
-const maxSize = Math.min(width, height);
-
-const margins = {
-    top: 10,
-    right: 10,
-    bottom: 10,
-    left: 10
-}
+const container = d3.select("#grid");
 
 // set the ranges
-const x = d3.scaleLinear().range([margins.left, maxSize - margins.right]);
-const y = d3.scaleLinear().range([maxSize - margins.bottom, margins.top]);
 const color = d3.scaleSequential(d3.interpolateYlOrRd);
 
 const tooltip = d3.select(".tooltip");
@@ -27,48 +12,48 @@ const now = new Date();
 const path = `data/processed/${now.getFullYear()}/${now.getMonth()+1}/${now.getDate()}/de.json`;
 
 d3.json(path).then(function(data) {
-    x.domain(d3.extent(data, (d) => {return d.embedding[0]; }))
-    y.domain(d3.extent(data, (d) => {return d.embedding[1]; }))
     color.domain(d3.extent(data, (d) => {return d.timestamp; }))
-    
-    const g = svg.append("g")
 
-    g.selectAll("circle")
+    container.selectAll(".item")
         .data(data)
         .enter()
-        .append("circle")
-        .attr("r", 5)
-        .attr("cx", d => x(d.embedding[0]))
-        .attr("cy", d => y(d.embedding[1]))
-        .style("fill", d => color(d.timestamp))
-        .on("mouseover", (event, d) => {
-            tooltip.style("left", (event.pageX) + "px")
-                   .style("top", (event.pageY - 28) + "px");
-            newsTitle.html(d.title);
-            newsSummary.html(d.summary);
+        .append("div")
+        .attr("class", "item")
+        .style("grid-column", d => d.embedding[0]+1)
+        .style("grid-row", d => d.embedding[1]+1)
+        .style("border-color", d => color(d.timestamp))
+        .html(d => `
+            <h1 class="news-title">${d.title}</h1>
+            <p class="news-summary">${d.summary}</p>
+        `)
+        // .on("mouseover", (event, d) => {
+        //     tooltip.style("left", (event.pageX) + "px")
+        //            .style("top", (event.pageY - 28) + "px");
+        //     newsTitle.html(d.title);
+        //     newsSummary.html(d.summary);
             
-            tooltip.transition()
-                .duration(200)
-                .style("opacity", 1.0);
+        //     tooltip.transition()
+        //         .duration(200)
+        //         .style("opacity", 1.0);
             
-        })
-        .on("mouseout", function(event, d) {
-            tooltip.transition()
-                .duration(500)
-                .style("opacity", 0);
-        })
+        // })
+        // .on("mouseout", function(event, d) {
+        //     tooltip.transition()
+        //         .duration(500)
+        //         .style("opacity", 0);
+        // })
         .on("click", function(event, d) {
             const win = window.open(d.link, "_blank");
             win.focus();
         });
      
-    svg.call(d3.zoom()
-        .extent([[0, 0], [width, height]])
-        .scaleExtent([1, 8])
-        .translateExtent([[-width+100, -height+100], [2*width-100, 2*height-100]])
-        .on("zoom", zoomed));
+    // container.call(d3.zoom()
+    //     .extent([[0, 0], [100, 100]])
+    //     .scaleExtent([1, 8])
+    //     // .translateExtent([[-width+100, -height+100], [2*width-100, 2*height-100]])
+    //     .on("zoom", zoomed));
   
-    function zoomed({transform}) {
-      g.attr("transform", transform);
-    }
+    // function zoomed({transform}) {
+    //   container.style("transform", transform);
+    // }
 });
